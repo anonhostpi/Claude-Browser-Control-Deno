a5c5620c20aa7c0bf5f1cdf925464a45704ca8f9
Blue Falcon <anon@hostpi.onmicrosoft.com>
2025-12-03 08:37:52 -0700

    feat(cdp): add module exports

---
+++ src/cdp/mod.ts
@@ -0,0 +1,8 @@
+/**
+ * CDP Module Exports
+ */
+
+export * from "./types.ts";
+export * from "./version.ts";
+export * from "./targets.ts";
+export { connect, connectToPage, type CDPClient } from "./client.ts";
