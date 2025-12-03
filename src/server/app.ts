9a1841367d5d670b39db7487190947946c070d05
Blue Falcon <anon@hostpi.onmicrosoft.com>
2025-12-03 10:05:55 -0700

    feat(server): add app composition

---
+++ src/server/app.ts
@@ -0,0 +1,19 @@
+/**
+ * Server App
+ * Composes routes into Hono application
+ */
+
+import { Hono } from "hono";
+import { rootRoutes, browserRoutes, instanceRoutes, targetRoutes } from "./routes/mod.ts";
+
+export function createApp(): Hono {
+  const app = new Hono();
+
+  // Mount routes
+  app.route("/", rootRoutes);
+  app.route("/", browserRoutes);
+  app.route("/", instanceRoutes);
+  app.route("/", targetRoutes);
+
+  return app;
+}
