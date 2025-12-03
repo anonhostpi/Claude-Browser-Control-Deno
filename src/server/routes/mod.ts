d83b0b0a008b478e30b3f26fe956a0098c1ecd45
Blue Falcon <anon@hostpi.onmicrosoft.com>
2025-12-03 10:05:35 -0700

    feat(server): add routes module exports

---
+++ src/server/routes/mod.ts
@@ -0,0 +1,8 @@
+/**
+ * Routes Module Exports
+ */
+
+export { rootRoutes } from "./root.ts";
+export { browserRoutes } from "./browser.ts";
+export { instanceRoutes } from "./instance.ts";
+export { targetRoutes } from "./target.ts";
