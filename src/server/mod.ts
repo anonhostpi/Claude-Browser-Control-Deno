b561ca0c2650f5775889fe4e99e78d58944edd37
Blue Falcon <anon@hostpi.onmicrosoft.com>
2025-12-03 10:06:33 -0700

    feat(server): add module exports

---
+++ src/server/mod.ts
@@ -0,0 +1,8 @@
+/**
+ * Server Module Exports
+ */
+
+export * from "./types.ts";
+export { registry } from "./registry.ts";
+export { createApp } from "./app.ts";
+export { startServer } from "./serve.ts";
